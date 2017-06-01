9c879c51511b628327e2ef0a65627f471081be18
min_xu <xumin_wlt@163.com>
2017-06-01 23:17:41 +0800

    add spring-web start pom

--- j360-disboot-web/src/main/java/me/j360/disboot/web/bootstrap/BootstrapConfiguration.java
+++ j360-disboot-web/src/main/java/me/j360/disboot/web/bootstrap/BootstrapConfiguration.java
@@ -1,10 +1,14 @@
 package me.j360.disboot.web.bootstrap;
 
+import org.springframework.context.annotation.Configuration;
+
 /**
  * Package: me.j360.disboot.web.bootstrap
  * User: min_xu
  * Date: 2017/6/1 下午10:54
  * 说明：
  */
+
+@Configuration
 public class BootstrapConfiguration {
 }
