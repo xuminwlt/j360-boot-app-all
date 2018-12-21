bc25704b20a589fc3fe111faefbd7cf464bcd672
min_xu <xumin_wlt@163.com>
2018-12-21 10:26:44 +0800

    modify annotation

---
+++ j360-disboot-biz/src/main/java/me/j360/disboot/bootstrap/BootstrapApplication1.java
@@ -0,0 +1,26 @@
+package me.j360.disboot.bootstrap;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * Package: me.j360.disboot.bootstrap
+ * User: min_xu
+ * Date: 2017/6/1 下午6:02
+ * 说明：
+ */
+
+@ComponentScan({"me.j360.disboot"})
+@MapperScan("me.j360.disboot.mapper")
+@EnableTransactionManagement
+@SpringBootApplication
+public class BootstrapApplication1 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootstrapApplication1.class, args);
+    }
+
+}
